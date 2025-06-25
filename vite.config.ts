05aad319b6aed52c6722b73894c260644b6914b2
Aidar Gallyamov <agallyamow@gmail.com>
2025-06-25 15:38:26 +0500

    add github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,32 @@ import SVG from 'vite-plugin-svgr';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig(() => ({
-	plugins: [
-		tsconfigPaths(),
-		react(),
-		SVG({
-			svgrOptions: {
-				exportType: 'default',
-				ref: true,
-				svgo: false,
-				titleProp: true,
-			},
-			include: 'src/**/*.svg',
-		}),
-		checker({ typescript: true }),
-	],
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, 'src'),
-			'@app/*': resolve(__dirname, 'src/app'),
-			'@pages/*': resolve(__dirname, 'src/pages'),
-			'@widgets/*': resolve(__dirname, 'src/widgets'),
-			'@features/*': resolve(__dirname, 'src/features'),
-			'@entities/*': resolve(__dirname, 'src/entities'),
-			'@shared/*': resolve(__dirname, 'src/shared'),
-			'@modules/*': resolve(__dirname, 'src/modules'),
-		},
-		extensions: ['.js', '.ts', '.jsx', '.tsx'],
-	},
+  base: '/logi-light/',
+  plugins: [
+    tsconfigPaths(),
+    react(),
+    SVG({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: 'src/**/*.svg',
+    }),
+    checker({ typescript: true }),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@app/*': resolve(__dirname, 'src/app'),
+      '@pages/*': resolve(__dirname, 'src/pages'),
+      '@widgets/*': resolve(__dirname, 'src/widgets'),
+      '@features/*': resolve(__dirname, 'src/features'),
+      '@entities/*': resolve(__dirname, 'src/entities'),
+      '@shared/*': resolve(__dirname, 'src/shared'),
+      '@modules/*': resolve(__dirname, 'src/modules'),
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
+  },
 }));
